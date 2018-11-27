bf33c231eeaec024694ea1bf6ad946ec1e860afe
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-27 20:25:11 +0100

    Add monte_carlo_simulation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # random-walk
 
-Imagine a person walking around in a city. What is the longest random walk the person can do so that they on average you will end up n blocks or fewer from home?
+Imagine a person walking around in a city. What is the longest random walk the person can do so that they on average you will end up 5 blocks or fewer from home?
--- random_walk.py
+++ random_walk.py
@@ -10,7 +10,7 @@ CARDINAL_DIRECTIONS = (
 
 
 def random_walk(n):
-    """Return coordinates after n step random walk"""
+    """Return coordinates after 'n' step random walk"""
     x, y = 0, 0
 
     for _ in range(n):
@@ -20,7 +20,33 @@ def random_walk(n):
     return x, y
 
 
-# Try 25 times
-for i in range(1, 25):
-    walk = random_walk(i)
-    print(f'{walk} Distance from home {abs(walk[0]) + abs(walk[1])}')
+def monte_carlo_simulation(number_of_walks, walk_length, block_distance):
+    """Return number of random walks that end within 'block_distance' of starting point"""
+    no_transport = 0
+
+    for walk in range(number_of_walks):
+        x, y = random_walk(walk_length)
+        distance = abs(x) + abs(y)
+
+        if distance <= block_distance:
+            no_transport += 1
+
+    return no_transport
+
+
+def main():
+    block_distance = 5
+    number_of_walks = 25_000
+
+    print('Running {} rounds of monte carlo simulation per walk length...')
+    print(f'Walk len | % closer than {block_distance} blocks')
+    # Do walks with from block_distance to 25 steps
+    # block_distance steps and less will always end within block_distance
+    for walk_length in range(block_distance, 25):
+        no_transport = monte_carlo_simulation(number_of_walks, walk_length, block_distance)
+        no_transport_percentage = 100 * no_transport / number_of_walks
+        print(f'     {walk_length}   |   {no_transport_percentage:.2f}')
+
+
+if __name__ == '__main__':
+    main()
